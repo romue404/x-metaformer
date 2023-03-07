dac711c5a54ef4460ad882e0bc2470fdd75b31bb
Robert Müller <robert.mueller1990@googlemail.com>
2023-03-07 15:53:31 +0100

    Feat: Added exclude_from_wt_decay

---
+++ misc.py
@@ -0,0 +1,16 @@
+def exclude_from_wt_decay(named_params, weight_decay, skip_list=("temp", "temperature", "scale", "norm")):
+    params = []
+    excluded_params = []
+
+    for name, param in named_params:
+        if not param.requires_grad:
+            continue
+        if any(layer_name in name for layer_name in skip_list):
+            excluded_params.append(param)
+            # print(f"skipped param {name}")
+        else:
+            params.append(param)
+    return [
+        {"params": params, "weight_decay": weight_decay},
+        {"params": excluded_params, "weight_decay": 0.0},
+    ]
